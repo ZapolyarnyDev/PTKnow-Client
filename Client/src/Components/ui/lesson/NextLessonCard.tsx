50cb891da8a3784848d95e9c2fce341764cd3672
IsMirakl <135332620+IsMirakl@users.noreply.github.com>
2026-03-17 23:39:48 +0300

    feat(ui): new components Next lesson Card

---
+++ Client/src/Components/ui/lesson/NextLessonCard.tsx
@@ -0,0 +1,26 @@
+import styles from '../../../styles/components/NextLessonCard.module.css'
+
+export const NextLessonCard = () => {
+  return (
+    <div className={styles.wrapper}>
+      <h2>Следующее занятие</h2>
+
+      <div className={styles.card}>
+        <h3>Введение, безопасность</h3>
+
+        <p className={styles.meta}>
+          Среда, 1 октября, 17:00–20:00
+        </p>
+
+        <p className={styles.desc}>
+          Знакомство с курсом, техника безопасности, основные компоненты роботов.
+        </p>
+
+        <div className={styles.bottom}>
+          <button>Добавить напоминание</button>
+          <span>Записано: 36</span>
+        </div>
+      </div>
+    </div>
+  );
+};
